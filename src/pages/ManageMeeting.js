7808b699b88b0ef002fb3643530801cfe84a5014
afifbashair <mafifbashair@gmail.com>
2026-05-20 09:00:10 +0700

    update: locLDttime

--- src/pages/ManageMeeting.js
+++ src/pages/ManageMeeting.js
@@ -14,15 +14,7 @@ import "../styles/admin.css";
 const formatDateTimeLocal = (dateTime) => {
   if (!dateTime) return "";
 
-  const date = new Date(dateTime);
-
-  const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, "0");
-  const day = String(date.getDate()).padStart(2, "0");
-  const hours = String(date.getHours()).padStart(2, "0");
-  const minutes = String(date.getMinutes()).padStart(2, "0");
-
-  return `${year}-${month}-${day}T${hours}:${minutes}`;
+  return dateTime.slice(0, 16);
 };
 export default function ManageMeeting() {
 
